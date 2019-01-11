f9502026cc1d29366460d467ab0ccdfd16aff103
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-01-11 12:54:54 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,13 +4,14 @@ def hello_t(array)
     yield(array[i])
     i += 1
   end
+  array
 end
 
 # call your method here!
 
 
-    hello_t(["Tim", "Tom", "Jim"]) do |name|
-      if name.start_with?("T")
-        puts "Hi, #{name}"
-      end
+  hello_t(["Tim", "Tom", "Jim"]) do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
     end
+  end
